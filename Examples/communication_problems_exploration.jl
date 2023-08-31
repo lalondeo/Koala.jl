dc99d93bfa8e7b2e66582479b56d6d90b9a6c4a6
Vincent Quirion <quirionv@arcade03.iro.umontreal.ca>
2023-08-31 12:29:44 -0400

    Minor tweaks in example code

--- Examples/communication_problems_exploration.jl
+++ Examples/communication_problems_exploration.jl
@@ -1,11 +1,13 @@
 using ArgParse
 using Base.Threads
 using Combinatorics
-using Dates
-# using ProgressMeter
+using LinearAlgebra
+using Serialization
+using ProgressMeter
 
-include("../koala.jl")
+BLAS.set_num_threads(1)
 
+include("../src/koala.jl")
 
 function get_functions(n_X::Int, n_Y::Int)
     nbits_X, nbits_Y = trunc(Int, log2(n_X)), trunc(Int, log2(n_Y))
@@ -52,31 +54,31 @@ end
 struct SingleExperimentResults
     index_within_process_id::Int64
     
-    experiment_start_time::DateTime
-    experiment_end_time::DateTime
+    experiment_start_time::Float64
+    experiment_end_time::Float64
 
     problem::Koala.Problems.OneWayCommunicationProblem
     distribution::Union{Matrix{Float64}, Nothing}
 
     classical_strat_lower_bound::Float64
     yao_strat_lower_bound::Float64
-    cleve_buhrman_strat_lower_bound::Float64
+
     cleve_buhrman_strat_upper_bound_GENERAL::Float64
     cleve_buhrman_strat_upper_bound_EPR::Float64
 
     skipped::Bool
 
-    function SingleExperimentResults(index_within_process_id::Int64, experiment_start_time::DateTime, experiment_end_time::DateTime, problem::Koala.Problems.OneWayCommunicationProblem, distribution::Matrix{Float64},
-        classical_strat_lower_bound::Float64, yao_strat_lower_bound::Float64, cleve_buhrman_strat_lower_bound::Float64, cleve_buhrman_strat_upper_bound_GENERAL:: Float64, cleve_buhrman_strat_upper_bound_EPR::Float64; skipped=false)
-        new(index_within_process_id, experiment_start_time, experiment_end_time, problem, distribution, classical_strat_lower_bound, yao_strat_lower_bound, cleve_buhrman_strat_lower_bound, cleve_buhrman_strat_upper_bound_GENERAL, cleve_buhrman_strat_upper_bound_EPR; skipped)
+    function SingleExperimentResults(index_within_process_id::Int64, experiment_start_time::Float64, experiment_end_time::Float64, problem::Koala.Problems.OneWayCommunicationProblem, distribution::Union{Matrix{Float64}, Nothing},
+        classical_strat_lower_bound::Float64, yao_strat_lower_bound::Float64, cleve_buhrman_strat_upper_bound_GENERAL:: Float64, cleve_buhrman_strat_upper_bound_EPR::Float64; skipped::Bool=false)
+        new(index_within_process_id, experiment_start_time, experiment_end_time, problem, distribution, classical_strat_lower_bound, yao_strat_lower_bound, cleve_buhrman_strat_upper_bound_GENERAL, cleve_buhrman_strat_upper_bound_EPR, skipped)
     end
 
-    function SingleExperimentResults(index_within_process_id::Int64, experiment_start_time::DateTime, experiment_end_time::DateTime, problem::Koala.Problems.OneWayCommunicationProblem)
-        SingleExperimentResults(index_within_process_id, experiment_start_time, experiment_end_time, problem, Nothing, 1, 1, 1, 1, 1; skipped=true)
+    function SingleExperimentResults(index_within_process_id::Int64, experiment_start_time::Float64, experiment_end_time::Float64, problem::Koala.Problems.OneWayCommunicationProblem)
+        SingleExperimentResults(index_within_process_id, experiment_start_time, experiment_end_time, problem, Nothing(), 1., 1., 1., 1.; skipped=true)
     end
 end
 
-mutable struct ProcessExperimentsResults
+struct SplitExperimentsResults
     total_processes::Int64
     process_id::Int64
     total_assigned_problems_to_process::Int64
@@ -84,12 +86,10 @@ mutable struct ProcessExperimentsResults
     start_index::Int64
     last_index::Int64
 
-    start_time::DateTime
-    last_update_time::DateTime
+    start_time::Float64
+    last_update_time::Float64
 
     results::Vector{SingleExperimentResults}
-
-    
 end
 
 
@@ -109,10 +109,10 @@ function get_command_line_args()
             help = "Size of C, the set of possible communications between Alice and Bob"
             arg_type = Int
             required = true
-        "--e-dim", "-e"
-            help = "Local dimension of the entangled quantum state for entangled strategies"
-            arg_type = Int
-            required = true
+        # "--e-dim", "-e"
+        #     help = "Local dimension of the entangled quantum state for entangled strategies"
+        #     arg_type = Int
+        #     required = true
         "--npa-lvl", "-l"
             help = "Level in the NPA hierarchy of the semi-definite program to compute the higher bound of the success probability of the entangled strategies."
             arg_type = Int
@@ -126,12 +126,7 @@ function get_command_line_args()
             help = "Process id, used to assign problems to processes"
             arg_type = Int
             required = true
-        "--checkpoint_time_interval"
-            help = "Time interval (in seconds) between each checkpoint save. *This is the maximum of compute time you will loose if the program crashes."
-            arg_type = Int
-            default = 5 * 60
-            required = false
-        "--results_dir", "-d"
+        "--results-dir", "-d"
             help = "Directory where the program will save the results. *Make sure the program has write permissions to it"
             arg_type = String
             default = "./"
@@ -143,12 +138,11 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__
     args = get_command_line_args()
 
-    n_X, n_Y, c, e_dim, npa_lvl, n_processes, process_id = args["x-size"], args["y-size"], args["c-size"], args["e-dim"], args["npa-lvl"], args["n-processes"], args["process-id"]
+    n_X, n_Y, c, npa_lvl, n_processes, process_id, dir = args["x-size"], args["y-size"], args["c-size"], args["npa-lvl"], args["n-processes"], args["process-id"], args["results-dir"]
 
     @time begin
 
         F = get_functions(n_X, n_Y)
-        # progress_bar = Progress(length(F), showspeed=true)
         # processed_counts = zeros(Threads.nthreads())
         # skipped_counts = zeros(Threads.nthreads())
         assignments = [[] for i=1:n_processes]
@@ -158,7 +152,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
         # assigned_counts = [size(assignments[i]) for i in 1:Threads.nthreads()]
     end
     @time begin
-    #     Threads.@threads :static for threadid in 1:Threads.nthreads()
+    
         problem = Koala.Problems.OneWayCommunicationProblem(n_X, n_Y, c, (x, y) -> true)
 
         game = Koala.Problems.gameify(problem)
@@ -166,61 +160,102 @@ if abspath(PROGRAM_FILE) == @__FILE__
         perfect_classical_strat_info = Koala.Strategies.HasPerfectClassicalStrategyInfo(game)
         classical_solver_data = Koala.Strategies.ClassicalSolverData(game)
         yao_solver_data = Koala.Strategies.YaoSolverData(n_X, n_Y, c)
-        entangled_solver_data = Koala.Strategies.EntangledSolverData(game, e_dim)
+        # entangled_solver_data = Koala.Strategies.EntangledSolverData(game, e_dim)
 
         npa_info = Koala.Bounds.NPAGeneral(problem, npa_lvl)
+        npa_epr_info = Koala.Bounds.NPAGeneral(problem, npa_lvl; impose_maximally_entangled=true)
+
 
         classical_strat = Koala.Strategies.ClassicalStrategy(game)
         yao_strategy = Koala.Strategies.YaoProtocol(problem)
-        entangled_strat = Koala.Strategies.EntangledStrategy(game, e_dim)
+        # entangled_strat = Koala.Strategies.EntangledStrategy(game, e_dim)
 
         converted_dist = [0.0 for x=1:n_X, y=1:(n_Y * c)]
+
+
+        split_start_time = time()
+        ended_time = 0
+
+        progress_bar = Progress(length(assignments[process_id]), showspeed=true)
     
-        for func in assignments[process_id]
-            update_problem!(problem, n_X, n_Y, func)
-            
-            game = Koala.Problems.gameify(problem)
-
-            if Koala.Strategies.has_perfect_classical_strategy!(game, classical_strat, perfect_classical_strat_info)
-                # processed_counts[Threads.threadid()] += 1
-                # skipped_counts[Threads.threadid()] += 1
-                # next!(progress_bar, showvalues = [(:"Total functions processed", "$(progress_bar.counter + 1) / $(progress_bar.n)"), (:"Functions processsed per thread", processed_counts), (:"Functions assigned per thread", assigned_counts), (:"Skipped", skipped_counts)])
-                continue
-            end
-
-            yao_hard_dist = Koala.Strategies.generate_hard_distribution_yao(problem, yao_strategy, yao_solver_data; max_iter=50, verbose=false)
-
-            print(typeof(yao_hard_dist))
-
-            Koala.Problems.convert_distribution!(problem, yao_hard_dist, converted_dist)
-
-            Koala.Strategies.optimize_strategy!(game, classical_strat, converted_dist, classical_solver_data; max_iter=50)
-            p_c = Koala.Strategies.evaluate_success_probability(game, classical_strat, converted_dist)
-
-            Koala.Strategies.optimize_strategy!(game, entangled_strat, converted_dist, entangled_solver_data; max_iter=50)
-            p_e = Koala.Strategies.evaluate_success_probability(game, entangled_strat, converted_dist)
-
-            Koala.Strategies.optimize_strategy!(problem, yao_strategy, yao_hard_dist, yao_solver_data; max_iter=50)
-            p_y = Koala.Strategies.evaluate_success_probability(problem, yao_strategy, yao_hard_dist)
-
-            p_c_ub = Koala.Bounds.upper_bound_game(game, converted_dist, npa_info; target=1/2 + 1/2 * p_y + 0.01, decompose=true, rho=0.01, eps_abs = 1e-08, eps_rel = 1e-08, verbose=false) 
-            # p_c_ub = Koala.Bounds.upper_bound_game(game, converted_dist, npa_info; target=1/2 + 1/2 * p_y - 0.05, decompose=true, rho=0.01, verbose=true)
+        println(length(assignments[process_id]))
 
 
+        for (index, func) in enumerate(assignments[process_id])
+            # try
+                # if isfile("$dir$process_id-$index.results")
+                    # continue
+                # end
 
-            # println("yao : $(1/2 + 1/2 * p_y), intrication inf : $p_c, intrication sup: $p_c_ub")
+                if !isfile("$dir$process_id-$index.ERROR")
+                    continue
+                end
+                
+                println("index : $index")
 
+                start_time = time()
+                update_problem!(problem, n_X, n_Y, func)
+                
+                global game = Koala.Problems.gameify(problem)
 
-            # processed_counts[Threads.threadid()] += 1
-            # next!(progress_bar, showvalues = [(:"Total functions processed", "$(progress_bar.counter + 1) / $(progress_bar.n)"), (:"Functions processsed per thread", processed_counts), (:"Functions assigned per thread", assigned_counts), (:"Skipped", skipped_counts)])
-        
-            # if progress_bar.counter >= 100
-                # break
+                if Koala.Strategies.has_perfect_classical_strategy!(game, classical_strat, perfect_classical_strat_info)
+                    # processed_counts[Threads.threadid()] += 1
+                    # skipped_counts[Threads.threadid()] += 1
+                    # next!(progress_bar, showvalues = [(:"Total functions processed", "$(progress_bar.counter + 1) / $(progress_bar.n)"), (:"Functions processsed per thread", processed_counts), (:"Functions assigned per thread", assigned_counts), (:"Skipped", skipped_counts)])
+                    
+                    global ended_time = time() 
+
+                    open("$dir$process_id-$index.results", "w") do io
+                        serialize(io, SplitExperimentsResults(length(assignments), process_id, length(assignments[process_id]), index, index, time(), time(), [SingleExperimentResults(index, start_time, ended_time, deepcopy(problem))]))    
+                    end
+                    
+                    next!(progress_bar)
+                    continue
+                end
+                # println("dist")
+                # @time begin
+                    yao_hard_dist = Koala.Strategies.generate_hard_distribution_yao(problem, yao_strategy, yao_solver_data; max_iter=100, verbose=false)
+                # end
+                Koala.Problems.convert_distribution!(problem, yao_hard_dist, converted_dist)
+
+                # println("classical")
+                # @time begin
+                    Koala.Strategies.optimize_strategy!(game, classical_strat, converted_dist, classical_solver_data; max_iter=50)
+                    p_c = Koala.Strategies.evaluate_success_probability(game, classical_strat, converted_dist)
+                # end
+
+                # println("yao")
+                # @time begin
+                    Koala.Strategies.optimize_strategy!(problem, yao_strategy, yao_hard_dist, yao_solver_data; max_iter=50)
+                    p_y = Koala.Strategies.evaluate_success_probability(problem, yao_strategy, yao_hard_dist)
+                # end
+
+                # println("e")
+                # @time begin
+                    # Koala.Strategies.optimize_strategy!(game, entangled_strat, converted_dist, entangled_solver_data; max_iter=50)
+                    # p_e = Koala.Strategies.evaluate_success_probability(game, entangled_strat, converted_dist)
+                # end
+
+                # println("npa")
+                # @time begin
+                    p_e_ub = Koala.Bounds.upper_bound_game(game, converted_dist, npa_info; target=1/2 + 1/2 * p_y + 0.01, decompose=true, rho=0.01, eps_abs = 1e-08, eps_rel = 1e-08, max_iter=10000, verbose=false) 
+                # end
+                    p_e_ub_epr = Koala.Bounds.upper_bound_game(game, converted_dist, npa_epr_info; target=1/2 + 1/2 * p_y + 0.01, decompose=true, rho=0.01, eps_abs = 1e-08, eps_rel = 1e-08, max_iter=10000, verbose=false) 
+                global ended_time = time()
+
+                open("$dir$process_id-$index.results", "w") do io
+                    serialize(io, SplitExperimentsResults(length(assignments), process_id, length(assignments[process_id]), index, index, time(), time(), [SingleExperimentResults(index, start_time, ended_time, deepcopy(problem), yao_hard_dist, p_c, p_y, p_e_ub, p_e_ub_epr)]))
+                end
+                next!(progress_bar)
+
             # end
-            print("a")
+            
+            # catch
+                # open("$dir$process_id-$index.ERROR", "w") do io
+                #     serialize(io, "")
+                # end
+                # next!(progress_bar)
+            # end
         end
-        # end
-        # println(skipped_counts)
-        # finish!(progress_bar)
     end
 end
